7340ab748a393dae29b873cc2ed4e891686981be
Julian Martinez <73849597+Julian-dev28@users.noreply.github.com>
2021-02-17 17:03:22 -0800

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,9 @@
 var KiwiCoin = artifacts.require('KiwiCoin');
 var KiwiCoinSale = artifacts.require('KiwiCoinSale');
 
-module.exports = function(deployer) {
-  deployer.deploy(KiwiCoin, 1000000).then(function() {
-    // Token price is 0.001 Ether
+module.exports = (deployer) => {
+  deployer.deploy(KiwiCoin, 1000000).then(()=> {
     var tokenPrice = 1000000000000000;
     return deployer.deploy(KiwiCoinSale, KiwiCoin.address, tokenPrice);
-  }
+  })
+}
